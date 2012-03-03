9defe0ef162b1a70da386a039716fa512853aaa3
liuhaonk@gmail.com <liuhaonk@gmail.com@c704f5a8-4941-0d45-8be1-f90bbcdc5bf8>
2012-03-03 14:08:46 +0000

    initial framework

---
+++ trunk/src/GameStateGamePlay.cpp
@@ -0,0 +1,14 @@
+#include "GameStateGamePlay.h"
+
+void GameStateGamePlay::enter ()
+{
+}
+void GameStateGamePlay::exit ()
+{
+}
+void GameStateGamePlay::pause ()
+{
+}
+void GameStateGamePlay::resume ()
+{
+}
