0947a9feffcd8a6ae3d54bdd6f3d9833910f6e2e
liuhaonk@gmail.com <liuhaonk@gmail.com@c704f5a8-4941-0d45-8be1-f90bbcdc5bf8>
2012-03-04 05:52:20 +0000

    executable demo

--- src/GameStateGamePlay.h
+++ src/GameStateGamePlay.h
@@ -1,10 +1,42 @@
 #ifndef _STATE_GAMEPLAY_H_
 #define _STATE_GAMEPLAY_H_
 
-#include "GameState.h"
+#include <Ogre.h>
+#include <OIS.h>
+#include <OgreCamera.h>
+#include <OgreEntity.h>
+#include <OgreLogManager.h>
+#include <OgreRoot.h>
+#include <OgreViewport.h>
+#include <OgreSceneManager.h>
+#include <OgreRenderWindow.h>
+#include <OgreConfigFile.h>
 
-class GameStateGamePlay : public GameState
+#include <OISEvents.h>
+#include <OISInputManager.h>
+#include <OISMouse.h>
+#include <OISKeyboard.h>
+
+#include "OpenGameArch.h"
+#include "GameState.h"
+#include "SinbadCharacterController.h"
+
+class GameStateGamePlay : public GameState, public OIS::MouseListener, public OIS::KeyListener, public Ogre::FrameListener, public Ogre::WindowEventListener
 {
+protected:
+	Ogre::String mArchive;
+	Ogre::String mMap;
+	OIS::Mouse* m_mouse;
+	OIS::Keyboard* m_keyboard;
+	SinbadCharacterController* mSinbad;
+	virtual bool mouseMoved(const OIS::MouseEvent& arg);
+	virtual bool mousePressed(const OIS::MouseEvent& arg, OIS::MouseButtonID id);
+	virtual bool mouseReleased(const OIS::MouseEvent& arg, OIS::MouseButtonID id);
+	virtual bool keyPressed(const OIS::KeyEvent& arg);
+	virtual bool keyReleased(const OIS::KeyEvent& arg);
+	virtual bool frameRenderingQueued(const Ogre::FrameEvent& arg);
+public:
+	GameStateGamePlay(Game* game);
 	void enter ();
 	void exit ();
 	void pause ();
